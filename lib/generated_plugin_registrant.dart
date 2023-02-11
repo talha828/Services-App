91933ec5d3ca8a72f835a868810079b61a347d9a
talha828 <talhaiqbal246@gmail.com>
2023-02-11 22:14:25 +0500

    first commit

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,47 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:audioplayers/web/audioplayers_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:printing/printing_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  AudioplayersPlugin.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FlutterFacebookAuthPlugin.registerWith(registrar);
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PrintingPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
